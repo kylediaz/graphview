53171bb355aed391d3abd2f418042914c0f57dfe
Kyle Diaz <dev@kylediaz.com>
2025-02-26 21:40:32 -0500

    fix: Make stored directories relative to project root

--- src/graphview/storage.py
+++ src/graphview/storage.py
@@ -8,6 +8,7 @@ from graphview.chunker import Chunk
 
 DATABASE_PATH = ".graphview/"
 
+
 @dataclass
 class SearchResult:
     file_path: Path
@@ -28,25 +29,35 @@ class Storage:
         result = self.collection.query(
             query_texts=[query], include=["metadatas", "distances"]
         )
-        ids = result['ids'][0]
-        metadatas = result['metadatas'][0]
-        distances = result['distances'][0]
+        ids = result["ids"][0]
+        metadatas = result["metadatas"][0]
+        distances = result["distances"][0]
 
         output = []
         for _, metadata, distance in zip(ids, metadatas, distances):
-            search_result = SearchResult(metadata['file_path'], metadata['line_number'], distance)
+            search_result = SearchResult(
+                metadata["file_path"], metadata["line_number"], distance
+            )
             output.append(search_result)
         return output
 
     def clear(self) -> None:
         # this is a hack. sorry :)
-        self.collection.delete(where={"path": { "$nin": [""]}})
+        self.collection.delete(where={"path": {"$nin": [""]}})
+
+    def count(self) -> int:
+        return self.collection.count
 
     def add(self, chunks: list[Chunk]) -> None:
         ids = [chunk.id() for chunk in chunks]
+
         metadatas = [
-            {"file_path": str(chunk.path), "line_number": str(chunk.line_number)} for chunk in chunks
+            {
+                "file_path": str(chunk.path.relative_to(Path.cwd())),
+                "line_number": str(chunk.line_number),
+            }
+            for chunk in chunks
         ]
         documents = [chunk.content for chunk in chunks]
-        
+
         self.collection.add(documents=documents, ids=ids, metadatas=metadatas)
