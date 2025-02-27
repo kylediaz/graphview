89c80539bd8b59cbc849e6aeb1a6a979c8471a38
Kyle Diaz <dev@kylediaz.com>
2025-02-26 19:37:30 -0500

    Init project

---
+++ README.md
@@ -0,0 +1,28 @@
+## Introduction
+
+`graphview` is a CLI tool to help you navigate your markdown notes. Running it will
+display a navigatable list of related notes.
+
+```bash
+graphview index build
+graphview search text "Vector databases"
+```
+
+Graphview works best when used with your favorite code editor. Use it as part of script to
+automatically search using what you currently have open in the editor.
+
+```bash
+graphview search file --line-number 50 my_note.md
+```
+
+## Zed
+
+Add an entry to `tasks.json` that executes the following command:
+
+```bash
+{
+  "label": "Local graph view current file",
+  "command": "graphview search file",
+  "args": ["--line-number", "$ZED_ROW", "$ZED_RELATIVE_FILE"]
+}
+```
---
+++ src/graphview/cli.py
@@ -0,0 +1,43 @@
+import typer
+from typing_extensions import Annotated
+
+app = typer.Typer(
+    help="A tool to help you navigate your markdown notes.", no_args_is_help=True
+)
+
+search_app = typer.Typer(
+    name="search", help="Search files using a query", no_args_is_help=True
+)
+app.add_typer(search_app, name="search")
+
+index_app = typer.Typer(
+    name="index",
+    help="Build or modify the index in this directory",
+    no_args_is_help=True,
+)
+app.add_typer(index_app, name="index")
+
+
+@search_app.command(
+    "file",
+    help="Search files based on the file at FILE_PATH with emphasis on the text at LINE_NUMBER",
+)
+def search_file(
+    file_path: Annotated[str, typer.Argument(help="Path to plain text document")],
+    line_number: int | None = None,
+):
+    typer.echo(f"file {file_path} {line_number}")
+
+
+@search_app.command("text", help="Search files based on the given QUERY")
+def search_text(query: str):
+    typer.echo("text")
+
+
+@index_app.command("build", help="Build or rebuild the index in this directory")
+def index_build():
+    typer.echo("index build")
+
+
+def main():
+    app()
